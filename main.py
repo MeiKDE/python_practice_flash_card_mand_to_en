3dec47088f173fa6d52711fcbf01f1aba6bd535e
Mei Zhang <rubymxzhang@gmail.com>
2024-12-03 15:33:09 +0800

    modified path name

--- main.py
+++ main.py
@@ -14,12 +14,12 @@ to_learn = {}
 
 try:
     data = pandas.read_csv(
-        "/Users/mei/projects/python_practice_flash_card_man_to_span_app_capstone/data/words_to_learn.csv"
+        "/Users/mei/projects/python_practice_flash_card_mand_to_span_app_capstone/data/words_to_learn.csv"
     )
     # print(data)
 except FileNotFoundError:
     original_data = pandas.read_csv(
-        "/Users/mei/projects/python_practice_flash_card_man_to_span_app_capstone/data/mandarin_english_spanish_words.csv"
+        "/Users/mei/projects/python_practice_flash_card_mand_to_span_app_capstone/data/mandarin_english_spanish_words.csv"
     )
     to_learn = original_data.to_dict(orient="records")
 else:
@@ -45,7 +45,7 @@ def is_known():
     to_learn.remove(current_card)
     data = pandas.DataFrame(to_learn)
     data.to_csv(
-        "/Users/mei/projects/python_practice_flash_card_man_to_span_app_capstone/data/words_to_learn.csv",
+        "/Users/mei/projects/python_practice_flash_card_mand_to_span_app_capstone/data/words_to_learn.csv",
         index=False,
     )
     next_card()
@@ -69,10 +69,10 @@ canvas = Canvas(width=800, height=526)
 
 # Front Card
 card_front_img = PhotoImage(
-    file="/Users/mei/projects/python_practice_flash_card_man_to_span_app_capstone/images/card_front.png"
+    file="/Users/mei/projects/python_practice_flash_card_mand_to_span_app_capstone/images/card_front.png"
 )
 card_back_img = PhotoImage(
-    file="/Users/mei/projects/python_practice_flash_card_man_to_span_app_capstone/images/card_back.png"
+    file="/Users/mei/projects/python_practice_flash_card_mand_to_span_app_capstone/images/card_back.png"
 )
 
 card_background = canvas.create_image(400, 263, image=card_front_img)
@@ -87,14 +87,14 @@ canvas.grid(row=0, column=0, columnspan=2)
 # Buttons
 
 cross_image = PhotoImage(
-    file="/Users/mei/projects/python_practice_flash_card_man_to_span_app_capstone/images/wrong.png"
+    file="/Users/mei/projects/python_practice_flash_card_mand_to_span_app_capstone/images/wrong.png"
 )
 
 unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
 unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(
-    file="/Users/mei/projects/python_practice_flash_card_man_to_span_app_capstone/images/right.png"
+    file="/Users/mei/projects/python_practice_flash_card_mand_to_span_app_capstone/images/right.png"
 )
 known_button = Button(image=check_image, highlightthickness=0, command=is_known)
 known_button.grid(row=1, column=1)
